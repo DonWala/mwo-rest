89665ceed6894cbf6984c18bfbd81a3e1810d3c6
Radoslaw Wala <wala.radoslaw@gmail.com>
2020-05-15 23:32:36 +0200

    repair MeetingService

--- src/main/java/com/company/enroller/persistence/MeetingService.java
+++ src/main/java/com/company/enroller/persistence/MeetingService.java
@@ -2,30 +2,52 @@ package com.company.enroller.persistence;
 
 import java.util.Collection;
 
+import com.company.enroller.model.Participant;
 import org.hibernate.Query;
+import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.springframework.stereotype.Component;
-
 import com.company.enroller.model.Meeting;
 
 @Component("meetingService")
 public class MeetingService {
 
-	DatabaseConnector connector;
 
+	Session session;
+		
 	public MeetingService() {
-		connector = DatabaseConnector.getInstance();
-	}
+		session = DatabaseConnector.getInstance().getSession();
+		}
 
 	public Collection<Meeting> getAll() {
 		String hql = "FROM Meeting";
-		Query query = connector.getSession().createQuery(hql);
+		Query query = this.session.createQuery(hql);
 		return query.list();
 	}
 	
 	public Meeting findById (long id) {
-		return Meeting  = (Meeting) connector.getSession(Meeting.class, id);
+		return (Meeting) this.session.get(Meeting.class, id);
 		
+	}
+	public Meeting add(Meeting meeting){
+		Transaction transaction = this.session.beginTransaction();
+		session.save(meeting);
+		transaction.commit();
+		return meeting;
+	}
+
+
+	public Meeting delete(Meeting meeting){
+		Transaction transaction = this.session.beginTransaction();
+		session.delete(meeting);
+		transaction.commit();
+		return meeting;
+	}
+
+	public void update(Meeting meeting) {
+		Transaction transaction = this.session.beginTransaction();
+		this.session.merge(meeting);
+		transaction.commit();
 	}
 
 }
